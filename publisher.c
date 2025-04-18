0947a15fd23ab485252f4954a7c1fe4de8b801c3
yu-heejin <jkgt0521@naver.com>
2025-04-18 13:42:47 +0900

    feat: 실행 시 이벤트 전송하는 pub 기능 추가

--- publisher.c
+++ publisher.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
+#include <hiredis/hiredis.h>
 
-int main(void) {
+int main(void)
+{
     redisContext *redis = redisConnect("127.0.0.1", 6379);
-
-    if (redis == NULL) {
-        printf("Can't allocate redis context\n");
-        return -1;
-    }
-    
-    if (redis->err) {
-        printf("Error: %s\n", redis->err);
-        return -1;
+    if (redis == NULL || redis->err) {
+        if (redis) {
+            printf("Error: %s\n", redis->errstr);
+        } else {
+            printf("Can't allocate redis context\n");
+        }
     }
 
-    redisReply *reply = redisCommand(redis, "")
+    // redis 특정 채널에 메시지 전송
+    const char *event_json = "{\"table\": \"board\", \"change\": \"insert\"}";
+
+    redisReply *reply = redisCommand(redis, "PUBLISH %s %s", "apt", event_json);
+    if (reply == NULL) {
+        printf("Can't publish command");
+    }
+
+    freeReplyObject(reply);
+    redisFree(redis);
 }
