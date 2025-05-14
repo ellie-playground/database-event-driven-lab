1cc1d608b25e39ca0c02292d8468b7f6b4a68fca
Yu Hee Jin <jkgt0521@naver.com>
2025-05-14 09:12:29 +0900

    feat: SQLite 연결 및 채널, 메시지 분기 로직 추가

--- publisher.c
+++ publisher.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <hiredis/hiredis.h>
+#include <sqlite3.h>
 
-int main(void)
-{
+int main(void) {
+    char query[1024];
+
+    // Redis connection
     redisContext *redis = redisConnect("127.0.0.1", 6379);
+
     if (redis == NULL || redis->err) {
         if (redis) {
             printf("Error: %s\n", redis->errstr);
@@ -12,14 +16,66 @@ int main(void)
         }
     }
 
-    // redis 특정 채널에 메시지 전송
-    const char *event_json = "{\"table\": \"board\", \"change\": \"insert\"}";
+    // SQL connection
+    sqlite3* db;
 
-    redisReply *reply = redisCommand(redis, "PUBLISH %s %s", "apt", event_json);
-    if (reply == NULL) {
-        printf("Can't publish command");
+    // :memory: 를 인자로 넘겨주면 인메모리 데이터베이스로 동작한다.
+    if (sqlite3_open(":memory:", &db) != SQLITE_OK) {
+        printf("Error: Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        redisFree(redis);
+        return -1;
     }
 
-    freeReplyObject(reply);
+    while (1) {
+        printf("Enter SQL Query: ");
+        fgets(query, sizeof(query), stdin);
+
+        // 1. 쿼리 실행
+
+        // 2. INSERT, UPDATE, DELETE 여부 확인
+        char event_message[512];
+        char channel[50];
+
+        if (strncasecmp(query, "INSERT", 6) == 0) {
+            char *table = strcasestr(query, "INTO");
+
+            if (table == NULL) {
+                printf("Error: You must Input table name. Please try again.\n");
+                continue;
+            }
+
+            snprintf(event_message, sizeof(event_message), "{\"table\": \"%s\", \"change\": \"insert\"}", table);
+            snprintf(channel, sizeof(channel), table);
+        } else if (strncasecmp(query, "UPDATE", 6) == 0) {
+            char *table = strcasestr(query, "UPDATE");
+
+            if (table == NULL) {
+                printf("Error: You must Input table name. Please try again.\n");
+                continue;
+            }
+
+            snprintf(event_message, sizeof(event_message), "{\"table\": \"%s\", \"change\": \"update\"}", table);
+            snprintf(channel, sizeof(channel), table);
+        } else if (strncasecmp(query, "DELETE", 6) == 0) {
+            char *table = strcasestr(query, "FROM");
+
+            if (table == NULL) {
+                printf("Error: You must Input table name. Please try again.\n");
+                continue;
+            }
+
+            snprintf(event_message, sizeof(event_message), "{\"table\": \"%s\", \"change\": \"delete\"}", table);
+            snprintf(channel, sizeof(channel), table);
+        }
+
+        redisReply *reply = redisCommand(redis, "PUBLISH %s %s", channel, event_message);
+        if (reply == NULL) {
+            printf("Can't publish command");
+        }
+
+        freeReplyObject(reply);
+    }
+
     redisFree(redis);
 }
